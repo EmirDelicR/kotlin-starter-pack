a46f17554e1d462fe219ed89a657543707debab4
EmirDelicR <emirdelictbf@gmail.com>
2024-04-07 13:38:28 +0200

    bugfix - Added comments to authService

--- api/src/main/kotlin/com/starter/api/rest/auth/core/AuthService.kt
+++ api/src/main/kotlin/com/starter/api/rest/auth/core/AuthService.kt
@@ -5,13 +5,23 @@ import com.starter.api.exception.NotFoundException
 import com.starter.api.rest.auth.dtos.LoginUserRequest
 import com.starter.api.rest.auth.dtos.RegisterUserRequest
 import com.starter.api.rest.roles.core.RoleService
+import com.starter.api.rest.users.core.CustomUserDetailsService
 import com.starter.api.rest.users.core.User
 import com.starter.api.rest.users.core.UserService
+import com.starter.api.utils.JWTHandler
 import com.starter.api.utils.PasswordEncoder
+import org.springframework.security.authentication.AuthenticationManager
+import org.springframework.security.core.token.TokenService
 import org.springframework.stereotype.Service
 
 @Service
-class AuthService(val userService: UserService, val roleService: RoleService) {
+class AuthService(
+    /*    private val authManager: AuthenticationManager,
+        private val userDetailsService: CustomUserDetailsService,
+        private val jwtHandler: JWTHandler,*/
+    private val userService: UserService,
+    private val roleService: RoleService
+) {
     fun registerUser(data: RegisterUserRequest): User {
         val user = userService.getByEmail(data.email)
 
@@ -20,7 +30,6 @@ class AuthService(val userService: UserService, val roleService: RoleService) {
         }
 
         val role = roleService.getByType()
-
         // TODO return refresh token or create function
         // const refreshToken = signToken(password, email, true);
 
@@ -33,6 +42,12 @@ class AuthService(val userService: UserService, val roleService: RoleService) {
                 ?: throw NotFoundException("User with email: (${data.email}) was not found!")
 
         PasswordEncoder.verifyPassword(data.password, user.password)
+
+        /*        val userDetails = userDetailsService.loadUserByUsername(data.email)
+                val token = jwtHandler.generateJwtToken(userDetails.username)
+                user.token = token*/
+
+        // userService.updateToken(user.id, token)
         // TODO @ed implement this
         // update user token
         // const refreshToken = signToken(password, email, true);
