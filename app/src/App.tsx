7ca874676cc35886e6e0515113b906a6f7cf0404
EmirDelicR <emirdelictbf@gmail.com>
2024-06-03 18:19:14 +0200

    feat: added notification to APP component

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -1,14 +1,17 @@
 import { MantineProvider } from "@mantine/core";
+import { Notifications } from "@mantine/notifications";
 
 import theme from "@/configs/themeConfig";
+import AppRoutes from "@/routes/Routes";
 
 import "@mantine/core/styles.css";
-import AppRoutes from "./routes/Routes";
+import "@mantine/notifications/styles.css";
 
 function App() {
   return (
     <MantineProvider theme={theme} defaultColorScheme="dark">
       <AppRoutes />
+      <Notifications />
     </MantineProvider>
   );
 }
