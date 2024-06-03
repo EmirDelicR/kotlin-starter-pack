0dee6fe7ac812f7ac7e12fb11971f47ce7473ce0
EmirDelicR <emirdelictbf@gmail.com>
2024-06-03 18:23:13 +0200

    feat: Added task manager

--- app/src/features/task/TaskManager.tsx
+++ app/src/features/task/TaskManager.tsx
@@ -4,134 +4,265 @@ import {
   Center,
   Group,
   List,
+  Loader,
+  LoadingOverlay,
   Modal,
+  Pagination,
   Paper,
   Stack,
   Text,
+  TextInput,
   Title,
 } from "@mantine/core";
+import { useDisclosure, useMediaQuery } from "@mantine/hooks";
+import { hasLength, useForm } from "@mantine/form";
+import { notifications } from "@mantine/notifications";
+
+import { useEffect, useMemo, useState } from "react";
+
 import {
   IconCircleCheck,
   IconCircleDashed,
   IconTrash,
 } from "@tabler/icons-react";
 
+import { useAppSelector } from "@/store";
+import { selectUserId } from "@/store/userSlice";
+
+import {
+  Task,
+  useAddTaskMutation,
+  useDeleteTaskMutation,
+  useGetPaginatedTasksQuery,
+  useUpdateTaskMutation,
+} from "./store";
+
+import { ITEMS_PER_PAGE, MOBILE } from "@/constants";
+import { normalizeError } from "@/utils";
+
+import { Error } from "@/UI/components/error/Error";
+
 import classes from "./TaskManager.module.scss";
-import { useDisclosure } from "@mantine/hooks";
 
-interface Item {
-  id: String;
-  title: String;
-  completed: Boolean;
-  userId: String;
-  createdAt: Date;
+type TaskForm = Pick<Task, "title">;
+interface TaskItemProps {
+  item: Task;
 }
-
-const ItemList: Item[] = [
-  {
-    id: "1",
-    title: "Task 1",
-    completed: true,
-    userId: "1",
-    createdAt: new Date(),
-  },
-  {
-    id: "2",
-    title: "Task 2",
-    completed: false,
-    userId: "1",
-    createdAt: new Date(),
-  },
-  {
-    id: "3",
-    title: "Task 3",
-    completed: true,
-    userId: "1",
-    createdAt: new Date(),
-  },
-  {
-    id: "4",
-    title: "Task 4",
-    completed: true,
-    userId: "1",
-    createdAt: new Date(),
-  },
-];
+const INITIAL_FORM_VALUES: TaskForm = {
+  title: "",
+};
 
 function CreateTaskForm() {
-  return <div>Task form</div>;
-  // const userId = useAppSelector(selectUserId);
-  // const [addTask, { isError, error, isSuccess }] = useAddTaskMutation();
-  // const { handleSubmit, control, resetField } = useForm<TaskForm>({
-  //   defaultValues: {
-  //     title: ''
-  //   },
-  //   mode: 'onChange'
-  // });
+  const userId = useAppSelector(selectUserId);
+  const [addTask, { isError, error, isSuccess, isLoading }] =
+    useAddTaskMutation();
+  const taskForm = useForm<TaskForm>({
+    mode: "uncontrolled",
+    initialValues: INITIAL_FORM_VALUES,
+    validate: {
+      title: hasLength(
+        { min: 2, max: 80 },
+        "Task title field is required to be between 2 and 80 chars."
+      ),
+    },
+  });
 
-  // useEffect(() => {
-  //   if (isSuccess) {
-  //     resetField('title');
-  //   }
-  // }, [isSuccess]);
+  useEffect(() => {
+    if (isSuccess) {
+      taskForm.reset();
+    }
+  }, [isSuccess]);
 
-  // const onSubmit = async (data: TaskForm) => {
-  //   await addTask({ userId, title: data.title });
-  // };
+  const handleSubmit = async (data: TaskForm) => {
+    await addTask({ userId, ...data });
+  };
 
-  // return (
-  //   <form onSubmit={handleSubmit(onSubmit)} className={classes.form} noValidate>
-  //     <Input
-  //       label="Enter new task"
-  //       name={'title'}
-  //       type="text"
-  //       data-testid="task-title"
-  //       control={control}
-  //       withAsterisk
-  //       useValidator={false}
-  //       rules={{
-  //         required: 'Task title field is required.',
-  //         maxLength: { value: 80, message: 'Task title is to long' }
-  //       }}
-  //     />
-  //     <Error isError={isError} error={error} />
-  //     <Button size="large" type="submit" className="submit">
-  //       Submit
-  //     </Button>
-  //   </form>
-  // );
+  return (
+    <Paper withBorder shadow="md" p="md" my="md" radius="md">
+      <LoadingOverlay
+        visible={isLoading}
+        zIndex={1000}
+        overlayProps={{ radius: "sm", blur: 2 }}
+        loaderProps={{ color: "var(--mantine-color-blue-6)", type: "bars" }}
+      />
+      <form onSubmit={taskForm.onSubmit(handleSubmit)}>
+        <TextInput
+          withAsterisk
+          label="Enter new task"
+          placeholder="Task title"
+          mb="md"
+          key={taskForm.key("title")}
+          {...taskForm.getInputProps("title")}
+        />
+
+        <Button fullWidth type="submit">
+          Submit
+        </Button>
+      </form>
+      <Error isError={isError} error={error} />
+    </Paper>
+  );
 }
 
-interface ItemProps {
-  item: Item;
-}
-
-function Item({ item }: ItemProps) {
+function UpdateTaskActionIcon({ item }: TaskItemProps) {
+  const [updateTask, { isError, error, isLoading }] = useUpdateTaskMutation();
+  const { message = "Unknown Error occurred!" } = normalizeError(error);
   const icon = item.completed ? (
     <IconCircleCheck size={16} />
   ) : (
     <IconCircleDashed size={16} />
   );
-
   const iconColor = item.completed ? "teal" : "blue";
 
+  const onUpdateHandler = async () => {
+    await updateTask({ ...item, completed: !item.completed });
+  };
+
+  useEffect(() => {
+    if (isError) {
+      notifications.show({
+        title: "Error updating task",
+        message: message,
+        color: "red",
+      });
+    }
+  }, [isError]);
+
+  return (
+    <ActionIcon
+      color={iconColor}
+      size={24}
+      radius="xl"
+      loading={isLoading}
+      onClick={onUpdateHandler}
+    >
+      {icon}
+    </ActionIcon>
+  );
+}
+
+function DeleteTaskActionIcon({ item }: TaskItemProps) {
+  const [deleteTask, { isError, error, isLoading }] = useDeleteTaskMutation();
+  const { message = "Unknown Error occurred!" } = normalizeError(error);
+
+  const onDeleteHandler = async () => {
+    await deleteTask({ taskId: item.id, userId: item.userId });
+  };
+
+  useEffect(() => {
+    if (isError) {
+      notifications.show({
+        title: "Error deleting task",
+        message: message,
+        color: "red",
+      });
+    }
+  }, [isError]);
+
+  return (
+    <ActionIcon
+      color="red"
+      size={24}
+      radius="xl"
+      onClick={onDeleteHandler}
+      loading={isLoading}
+    >
+      <IconTrash size={16} />
+    </ActionIcon>
+  );
+}
+
+function TaskItem({ item }: TaskItemProps) {
   return (
     <List.Item
       className={classes.item}
-      icon={
-        <ActionIcon color={iconColor} size={24} radius="xl">
-          {icon}
-        </ActionIcon>
-      }
+      icon={<UpdateTaskActionIcon item={item} />}
     >
       <Group justify="space-between">
         <Text>{item.title}</Text>
-        {/* // loading={true} */}
-        <ActionIcon color="red" size={24} radius="xl">
-          <IconTrash size={16} />
-        </ActionIcon>
+        <DeleteTaskActionIcon item={item} />
       </Group>
     </List.Item>
+  );
+}
+
+function TaskList() {
+  const userId = useAppSelector(selectUserId);
+  const [currentPage, setCurrentPage] = useState(0);
+  const isMobileView = useMediaQuery(MOBILE);
+  const { data, isLoading, isSuccess, isError, error } =
+    useGetPaginatedTasksQuery({
+      userId,
+      page: currentPage,
+      pageSize: ITEMS_PER_PAGE,
+      isMobile: isMobileView,
+    });
+
+  const content = useMemo(() => {
+    if (!isSuccess) {
+      return null;
+    }
+    const { tasks, numberOfPages } = data;
+
+    if (tasks && tasks.length > 0) {
+      return tasks.map((task: Task) => {
+        return <TaskItem key={task.id} item={task} />;
+      });
+    }
+
+    // This is in case that user delete last item on page
+    if (tasks.length === 0 && numberOfPages > 0) {
+      setCurrentPage((prev) => prev - 1);
+    }
+
+    return <Text>Currently there is no data.</Text>;
+  }, [data, isSuccess]);
+
+  if ((isLoading || !data) && !isError) {
+    return (
+      <Center my="lg">
+        <Loader type="bars" />
+      </Center>
+    );
+  }
+
+  if (isError) {
+    return <Error isError={isError} error={error} />;
+  }
+
+  const { numberOfPages } = data;
+
+  const onNext = () =>
+    setCurrentPage((prev) => {
+      if (prev > numberOfPages - 1) {
+        return prev;
+      }
+      return prev + 1;
+    });
+
+  return (
+    <>
+      <List spacing="md" size="sm">
+        {content}
+      </List>
+      <Center mt="md">
+        {isMobileView ? (
+          <Button
+            disabled={currentPage === numberOfPages}
+            onClick={onNext}
+            variant="outline"
+          >
+            Load more
+          </Button>
+        ) : (
+          <Pagination
+            total={numberOfPages}
+            onChange={setCurrentPage}
+            value={currentPage}
+          />
+        )}
+      </Center>
+    </>
   );
 }
 
@@ -145,11 +276,7 @@ export default function TaskManager() {
             <Title order={3}>Manage tasks</Title>
           </Center>
           <Paper withBorder shadow="md" p="md">
-            <List spacing="md" size="sm">
-              {ItemList.map((item) => (
-                <Item item={item} />
-              ))}
-            </List>
+            <TaskList />
           </Paper>
           <Button onClick={open}>Create</Button>
         </Stack>
---
+++ app/src/features/task/store/taskApiSlice.ts
@@ -0,0 +1,89 @@
+import { ITEMS_PER_PAGE } from "@/constants/api";
+import baseApi from "@/store/services/baseApiSetup";
+
+export interface Task {
+  userId: string;
+  id: string;
+  title: string;
+  completed: boolean;
+  createdAt: string;
+}
+
+interface ApiTaskResponse {
+  data: Task[];
+  message: string;
+  status: number;
+}
+
+interface ApiPaginatedTaskResponse {
+  data: {
+    tasks: Task[];
+    numberOfPages: number;
+  };
+  message: string;
+  status: number;
+}
+
+const baseApiWithTag = baseApi.enhanceEndpoints({ addTagTypes: ["Task"] });
+
+export const taskSlice = baseApiWithTag.injectEndpoints({
+  endpoints: (builder) => ({
+    getTasks: builder.query<Task[], null>({
+      query: () => "/tasks",
+      transformResponse: (res: ApiTaskResponse) => res.data,
+      providesTags: ["Task"],
+    }),
+
+    getPaginatedTasks: builder.query<
+      { tasks: Task[]; numberOfPages: number },
+      { userId: string; page?: number; pageSize?: number; isMobile?: boolean }
+    >({
+      query: ({
+        userId,
+        page = 0,
+        pageSize = ITEMS_PER_PAGE,
+        isMobile = false,
+      }) =>
+        `/tasks/paginated/${userId}?page=${page}&pageSize=${pageSize}&isMobile=${isMobile}`,
+      transformResponse: (res: ApiPaginatedTaskResponse) => res.data,
+      providesTags: ["Task"],
+    }),
+
+    addTask: builder.mutation({
+      query: (task: Partial<Task>) => ({
+        url: "/tasks",
+        method: "POST",
+        body: task,
+      }),
+      invalidatesTags: ["Task"],
+    }),
+
+    updateTask: builder.mutation({
+      query: (task: Task) => ({
+        url: `/tasks/${task.id}`,
+        method: "PUT",
+        body: task,
+      }),
+      invalidatesTags: ["Task"],
+    }),
+
+    deleteTask: builder.mutation({
+      query: ({ taskId, userId }: { taskId: string; userId: string }) => ({
+        url: `/tasks/${taskId}`,
+        method: "DELETE",
+        body: {
+          userId,
+        },
+      }),
+      invalidatesTags: ["Task"],
+    }),
+  }),
+});
+
+export const {
+  useGetTasksQuery,
+  useGetPaginatedTasksQuery,
+  useAddTaskMutation,
+  useUpdateTaskMutation,
+  useDeleteTaskMutation,
+} = taskSlice;
