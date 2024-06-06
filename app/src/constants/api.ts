6038f6777dcc1519104b8930fe94594b50311b89
EmirDelicR <emirdelictbf@gmail.com>
2024-06-06 20:19:46 +0200

    feat: added constants for pool interval

--- app/src/constants/api.ts
+++ app/src/constants/api.ts
@@ -26,3 +26,4 @@ enum HTTP_PORT {
 export const API_URL = `${HTTP_PROTOCOL.HTTP}://0.0.0.0:${HTTP_PORT.HTTP}/api/v1`;
 
 export const ITEMS_PER_PAGE = 5;
+export const POOL_INTERVAL_5_MINUTES = 1000 * 60 * 5;
