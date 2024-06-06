f6ae7df8c568dee99dcf98790250ed55e38bfab6
EmirDelicR <emirdelictbf@gmail.com>
2024-06-06 20:19:16 +0200

    bugfix: remove color settings

--- app/src/configs/themeConfig.ts
+++ app/src/configs/themeConfig.ts
@@ -1,22 +1,6 @@
 import { createTheme, rem } from "@mantine/core";
 
 export default createTheme({
-  colors: {
-    // or replace default theme color
-    blue: [
-      "#ebefff",
-      "#d5dafc",
-      "#a9b1f1",
-      "#7b87e9",
-      "#5362e1",
-      "#3a4bdd",
-      "#2d3fdc",
-      "#1f32c4",
-      "#182cb0",
-      "#0b259c",
-    ],
-  },
-
   headings: {
     fontFamily: "Roboto, sans-serif",
     sizes: {
