c8980ea4954fd85d3dc1c49529c0efa382e89a93
EmirDelicR <emirdelictbf@gmail.com>
2024-06-10 20:18:56 +0200

    feat: added cypress types and global cypress declaration

--- app/src/main.tsx
+++ app/src/main.tsx
@@ -8,11 +8,25 @@ import App from "./App";
 
 const container = document.getElementById("root") as HTMLElement;
 const root = createRoot(container!);
-
+const store = createStore();
 root.render(
   <React.StrictMode>
-    <Provider store={createStore()}>
+    <Provider store={store}>
       <App />
     </Provider>
   </React.StrictMode>
 );
+
+interface CypressWithStore extends Cypress.Cypress {
+  store?: typeof store;
+}
+
+declare global {
+  interface Window {
+    Cypress?: CypressWithStore;
+  }
+}
+
+if (typeof window !== "undefined" && window.Cypress) {
+  window.Cypress.store = store;
+}
