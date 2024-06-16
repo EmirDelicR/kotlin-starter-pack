7ad3b1131018739a01068f0736b71c69c5198618
EmirDelicR <emirdelictbf@gmail.com>
2024-06-16 14:14:17 +0200

    bugfix: Fix auth in open api to use token

--- api/src/main/kotlin/com/starter/api/config/SpringdocConfig.kt
+++ api/src/main/kotlin/com/starter/api/config/SpringdocConfig.kt
@@ -1,16 +1,20 @@
 package com.starter.api.config
 
 import io.swagger.v3.oas.annotations.OpenAPIDefinition
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeIn
 import io.swagger.v3.oas.annotations.enums.SecuritySchemeType
 import io.swagger.v3.oas.annotations.info.Info
+import io.swagger.v3.oas.annotations.security.SecurityRequirement
 import io.swagger.v3.oas.annotations.security.SecurityScheme
 import org.springframework.context.annotation.Configuration
 
 @Configuration
-@OpenAPIDefinition(info = Info(title = "Swagger application API", version = "v1"))
+@OpenAPIDefinition(info = Info(title = "Swagger application API", version = "v1"), security = [SecurityRequirement(name = "bearerAuth")])
 @SecurityScheme(
     type = SecuritySchemeType.HTTP,
-    name = "basicAuth",
-    scheme = "basic",
+    bearerFormat = "jwt",
+    name = "bearerAuth",
+    scheme = "bearer",
+    `in` = SecuritySchemeIn.HEADER,
 )
 class SpringdocConfig
