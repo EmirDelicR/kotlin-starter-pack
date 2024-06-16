5538752a51c69d38071c20b906a43c71c847554f
EmirDelicR <emirdelictbf@gmail.com>
2024-06-16 15:38:41 +0200

    bugfix: Fix cors for application

--- api/src/main/kotlin/com/starter/api/config/jwt/SecurityConfiguration.kt
+++ api/src/main/kotlin/com/starter/api/config/jwt/SecurityConfiguration.kt
@@ -6,16 +6,19 @@ import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.http.HttpMethod
 import org.springframework.security.authentication.AuthenticationProvider
+import org.springframework.security.config.Customizer
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity
 import org.springframework.security.config.annotation.web.builders.HttpSecurity
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
-import org.springframework.security.config.annotation.web.configurers.CorsConfigurer
-import org.springframework.security.config.annotation.web.configurers.CsrfConfigurer
 import org.springframework.security.config.annotation.web.configurers.ExceptionHandlingConfigurer
 import org.springframework.security.config.annotation.web.configurers.SessionManagementConfigurer
 import org.springframework.security.config.http.SessionCreationPolicy.STATELESS
 import org.springframework.security.web.DefaultSecurityFilterChain
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter
+import org.springframework.web.cors.CorsConfiguration
+import org.springframework.web.cors.CorsConfigurationSource
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource
+
 
 @Configuration
 @EnableWebSecurity
@@ -37,15 +40,35 @@ class SecurityConfiguration(private val authenticationProvider: AuthenticationPr
     private val unauthorizedHandler: EntryPoint? = null
 
     @Bean
+    fun corsConfigurationSource(): CorsConfigurationSource {
+        val configuration = CorsConfiguration()
+        configuration.allowedOrigins = listOf("http://localhost:3000")
+        configuration.allowedMethods = listOf(
+            "HEAD",
+            "GET", "POST", "PUT", "DELETE", "PATCH"
+        )
+        // setAllowCredentials(true) is important, otherwise:
+        // The value of the 'Access-Control-Allow-Origin' header in the response must not be the wildcard '*' when the request's credentials mode is 'include'.
+        configuration.allowCredentials = true
+        // setAllowedHeaders is important! Without it, OPTIONS preflight request
+        // will fail with 403 Invalid CORS request
+        configuration.allowedHeaders = listOf("Authorization", "Cache-Control", "Content-Type")
+        val source = UrlBasedCorsConfigurationSource()
+        source.registerCorsConfiguration("/**", configuration)
+        return source
+    }
+
+    @Bean
     @Throws(Exception::class)
     fun securityFilterChain(
         http: HttpSecurity,
         jwtAuthenticationFilter: AuthenticationFilter,
     ): DefaultSecurityFilterChain {
-        http.csrf { obj: CsrfConfigurer<HttpSecurity> -> obj.disable() }
-            .cors { obj: CorsConfigurer<HttpSecurity> -> obj.disable() }
+        http.csrf { it.disable() }
+            .cors(Customizer.withDefaults())
             .authorizeHttpRequests { req ->
-                req.requestMatchers(*whiteListUrl)
+                req.requestMatchers(HttpMethod.OPTIONS,"/api/v1/**").permitAll()
+                    .requestMatchers(*whiteListUrl)
                     .permitAll()
                     .requestMatchers(HttpMethod.POST, *arrayOf("/api/v1/subscriptions", "/api/v1/roles"))
                     .hasRole(RoleType.ADMIN.toString())
